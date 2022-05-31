9b0cae5e1941e049ccbdf7b3e0eef4fd30f4a290
Luis Quevedo <qfluis@gmail.com>
2022-05-31 12:46:05 +0200

    trabajando mapita

--- src/components/Mapita.js
+++ src/components/Mapita.js
@@ -1,52 +1,76 @@
 import L from 'leaflet';
-import { MapContainer, TileLayer, Marker, Popup  } from 'react-leaflet';
-import { useEffect } from 'react';
+import { MapContainer, TileLayer, Marker, Popup, useMap  } from 'react-leaflet';
+import { useEffect, useState } from 'react';
 import './Mapita.css';
+import markerPicture from './marker.png';
+
+const markerImg = L.icon({
+    iconUrl: markerPicture,
+    iconSize:[35,35]
+});
+
+const PopUp = () => {
+    return (
+        <>
+            <h4>Introduce nombre:</h4>
+            <input type="text" />
+        </>
+    );
+}
+
+const ControladorMapa = ({crearMarkerPopup}) => {
+    const map = useMap();
+    useEffect(()=>{
+        map.on("click", (event)=> {
+            crearMarkerPopup(event.latlng);
+        });
+    },[]);
+    
+    return null;
+}
+
 
 export const Mapita = () => {
 
-    var map;
-    
-    
-    useEffect(()=>{
-        
-        //map = L.map("mapita").setView([51.505, -0.09],13);
-        
-        /*
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(map);
-        
-        L.marker([51.5, -0.09]).addTo(map)
-        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-        .openPopup();
-        */
-       console.log("Holiwi");
-
-    },[]);
-    
+    var map;     
+          
     // TODO: https://react-leaflet.js.org/docs/start-introduction/
     // TODO: https://react-leaflet.js.org/docs/api-map/#mapcontainer
     
-    
+    const [markers, setMarkers] = useState ([]);
+
+    const crearMarkerPopup = (latlang) => {
+        setMarkers([...markers, latlang]);        
+    }
 
     return (
         <>
         <h1>Mapita</h1>
-        <MapContainer center={[41.40259959831558, 2.1945373890103843]} zoom={16}>
+        <MapContainer center={[41.40259959831558, 2.1945373890103843]} zoom={16}  >
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-
-            <Marker position={[41.40259959831558, 2.1945373890103843]}>
+            <ControladorMapa crearMarkerPopup={crearMarkerPopup} />
+            <Marker position={[41.40259959831558, 2.1945373890103843]} icon={markerImg} >
                 <Popup>
                     oLA K ASE!
                 </Popup>
             </Marker>
-
-
-
+            <Marker position={[41.40290, 2.19490]} icon={markerImg} eventHandlers={{
+                    click: () => {
+                    console.log('MAPA CLIK')
+                    },
+            }} >
+                <Popup>Mira la consola</Popup>
+            </Marker>
+            {
+                markers.map((m,i) => 
+                    <Marker key={i} position={m} icon={markerImg}>
+                        <Popup>Ola k ase</Popup>
+                    </Marker> 
+                )
+            }
         </MapContainer>
         </>
     ) 
